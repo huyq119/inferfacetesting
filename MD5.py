894b28aa08811e2e8855ad6f523ba75e5c7e6ddf
huyuqi <yuqi.hu@gmail.com>
2016-06-02 16:33:09 +0800

    add new files

---
+++ MD5.py
@@ -0,0 +1,14 @@
+import hashlib
+import types
+def md5(str):
+    if type(str) is types.StringType:
+        m = hashlib.md5()
+        m.update(str)
+        return m.hexdigest()
+    else:
+        return "ERROR"
+d = md5("1001001iOS,9.3.2,1280*1080cn.com.chinau.homepage0.5C3BofasVsQlfaj4R8KuPw7Jc")
+print d
+f = file('md5code.txt','r+')
+f.write(d)
+f.close()
